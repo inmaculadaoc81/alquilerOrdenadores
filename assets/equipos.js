71276e62a9cd88bd6fd37e0221aba739d5bd3a2f
inmaculadaoc81 <inmaculada.oc81@gmail.com>
2026-09-24 23:46:04 +0200

    Add Redsys rental buttons to equipment cards

--- assets/equipos.js
+++ assets/equipos.js
@@ -12,6 +12,33 @@
   var todosEquipos = [];
 
   var ETIQUETA_TIPO = { Portatil: "Portátil", Normal: "Sobremesa" };
+  var ENLACES_ALQUILER = {
+    "EQ-001": ["https://sis.redsys.es/tiendaWeb/item/NTY3OzI=", "https://sis.redsys.es/tiendaWeb/item/NTY3OzM=", "https://sis.redsys.es/tiendaWeb/item/NTY3OzQ="],
+    "EQ-002": ["https://sis.redsys.es/tiendaWeb/item/NTY3OzU=", "https://sis.redsys.es/tiendaWeb/item/NTY3OzY=", "https://sis.redsys.es/tiendaWeb/item/NTY3Ozc="],
+    "EQ-0027": ["https://sis.redsys.es/tiendaWeb/item/NTY3Ozg=", "https://sis.redsys.es/tiendaWeb/item/NTY3Ozk=", "https://sis.redsys.es/tiendaWeb/item/NTY3OzEw"],
+    "EQ-0028": ["https://sis.redsys.es/tiendaWeb/item/NTY3OzEx", "https://sis.redsys.es/tiendaWeb/item/NTY3OzEy", "https://sis.redsys.es/tiendaWeb/item/NTY3OzEz"],
+    "EQ-0029": ["https://sis.redsys.es/tiendaWeb/item/NTY3OzE0", "https://sis.redsys.es/tiendaWeb/item/NTY3OzE1", "https://sis.redsys.es/tiendaWeb/item/NTY3OzE2"],
+    "EQ-003": ["https://sis.redsys.es/tiendaWeb/item/NTY3OzE3", "https://sis.redsys.es/tiendaWeb/item/NTY3OzE4", "https://sis.redsys.es/tiendaWeb/item/NTY3OzE5"],
+    "EQ-0031": ["https://sis.redsys.es/tiendaWeb/item/NTY3OzIw", "https://sis.redsys.es/tiendaWeb/item/NTY3OzIx", "https://sis.redsys.es/tiendaWeb/item/NTY3OzIy"],
+    "EQ-004": ["https://sis.redsys.es/tiendaWeb/item/NTY3OzIz", "https://sis.redsys.es/tiendaWeb/item/NTY3OzI0", "https://sis.redsys.es/tiendaWeb/item/NTY3OzI1"],
+    "EQ-005": ["https://sis.redsys.es/tiendaWeb/item/NTY3OzI2", "https://sis.redsys.es/tiendaWeb/item/NTY3OzI3", "https://sis.redsys.es/tiendaWeb/item/NTY3OzI4"],
+    "EQ-006": ["https://sis.redsys.es/tiendaWeb/item/NTY3OzI5", "https://sis.redsys.es/tiendaWeb/item/NTY3OzMw", "https://sis.redsys.es/tiendaWeb/item/NTY3OzMx"],
+    "EQ-007": ["https://sis.redsys.es/tiendaWeb/item/NTY3OzMy", "https://sis.redsys.es/tiendaWeb/item/NTY3OzMz", "https://sis.redsys.es/tiendaWeb/item/NTY3OzM0"],
+    "EQ-008": ["https://sis.redsys.es/tiendaWeb/item/NTY3OzM1", "https://sis.redsys.es/tiendaWeb/item/NTY3OzM2", "https://sis.redsys.es/tiendaWeb/item/NTY3OzM3"],
+    "EQ-009": ["https://sis.redsys.es/tiendaWeb/item/NTY3OzM4", "https://sis.redsys.es/tiendaWeb/item/NTY3OzM5", "https://sis.redsys.es/tiendaWeb/item/NTY3OzQw"],
+    "EQ-010": ["https://sis.redsys.es/tiendaWeb/item/NTY3OzQx", "https://sis.redsys.es/tiendaWeb/item/NTY3OzQy", "https://sis.redsys.es/tiendaWeb/item/NTY3OzQz"],
+    "EQ-011": ["https://sis.redsys.es/tiendaWeb/item/NTY3OzQ0", "https://sis.redsys.es/tiendaWeb/item/NTY3OzQ1", "https://sis.redsys.es/tiendaWeb/item/NTY3OzQ2"],
+    "EQ-012": ["https://sis.redsys.es/tiendaWeb/item/NTY3OzQ3", "https://sis.redsys.es/tiendaWeb/item/NTY3OzQ4", "https://sis.redsys.es/tiendaWeb/item/NTY3OzQ5"],
+    "EQ-013": ["https://sis.redsys.es/tiendaWeb/item/NTY3OzUw", "https://sis.redsys.es/tiendaWeb/item/NTY3OzUx", "https://sis.redsys.es/tiendaWeb/item/NTY3OzUy"],
+    "EQ-014": ["https://sis.redsys.es/tiendaWeb/item/NTY3OzUz", "https://sis.redsys.es/tiendaWeb/item/NTY3OzU0", "https://sis.redsys.es/tiendaWeb/item/NTY3OzU1"],
+    "EQ-015": ["https://sis.redsys.es/tiendaWeb/item/NTY3OzU2", "https://sis.redsys.es/tiendaWeb/item/NTY3OzU3", "https://sis.redsys.es/tiendaWeb/item/NTY3OzU4"],
+    "EQ-016": ["https://sis.redsys.es/tiendaWeb/item/NTY3OzU5", "https://sis.redsys.es/tiendaWeb/item/NTY3OzYw", "https://sis.redsys.es/tiendaWeb/item/NTY3OzYx"],
+    "EQ-017": ["https://sis.redsys.es/tiendaWeb/item/NTY3OzYy", "https://sis.redsys.es/tiendaWeb/item/NTY3OzYz", "https://sis.redsys.es/tiendaWeb/item/NTY3OzY0"],
+    "EQ-018": ["https://sis.redsys.es/tiendaWeb/item/NTY3OzY1", "https://sis.redsys.es/tiendaWeb/item/NTY3OzY2", "https://sis.redsys.es/tiendaWeb/item/NTY3OzY3"],
+    "EQ-019": ["https://sis.redsys.es/tiendaWeb/item/NTY3OzY4", "https://sis.redsys.es/tiendaWeb/item/NTY3OzY5", "https://sis.redsys.es/tiendaWeb/item/NTY3Ozcw"],
+    "EQ-021": ["https://sis.redsys.es/tiendaWeb/item/NTY3Ozcx", "https://sis.redsys.es/tiendaWeb/item/NTY3Ozcy", "https://sis.redsys.es/tiendaWeb/item/NTY3Ozcz"],
+    "EQ-025": ["https://sis.redsys.es/tiendaWeb/item/NTY3Ozc0", "https://sis.redsys.es/tiendaWeb/item/NTY3Ozc1", "https://sis.redsys.es/tiendaWeb/item/NTY3Ozc2"]
+  };
 
   function escapeHtml(s) {
     return String(s == null ? "" : s)
@@ -41,6 +68,20 @@
         return '<span><b>' + precio + '</b> /' + etiqueta + "</span>";
       })
       .join("");
+    var links = ENLACES_ALQUILER[String(e.id_equipo || "").trim()];
+    var botonesAlquiler = "";
+    if (links && links.length === 3) {
+      var periodos = [
+        { texto: "Alquilar 1 día", precio: euros(e.dia) },
+        { texto: "Alquilar 1 semana", precio: euros(e.semanal) },
+        { texto: "Alquilar 1 mes", precio: euros(e.mensual) }
+      ];
+      botonesAlquiler = '<div class="equipo-alquiler-opciones">' + periodos.map(function (p, i) {
+        return '<a class="equipo-alquiler-btn" href="' + escapeHtml(links[i]) + '" target="_blank" rel="noopener noreferrer">' +
+          escapeHtml(p.texto) + (p.precio ? '<span>' + escapeHtml(p.precio) + '</span>' : '') + '</a>';
+      }).join("") + '</div>';
+    }
+
     return (
       '<article class="equipo-card">' +
       imagen +
@@ -48,6 +89,7 @@
       tipo +
       '<h4 class="equipo-nombre">' + escapeHtml(nombre) + "</h4>" +
       '<div class="equipo-precios">' + precios + "</div>" +
+      botonesAlquiler +
       "</div>" +
       '<a class="equipo-btn" href="https://api.whatsapp.com/send?phone=34649970128&text=' +
       encodeURIComponent("¡Hola! Me interesa alquilar el " + nombre) +
